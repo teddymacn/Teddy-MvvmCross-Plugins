f9d303beaf149320f8b02da1d596fb6f73506fb2
Matija Lukic <matija32@gmail.com>
2017-03-05 20:42:51 +0100

    implemented play on repeat extension method instead of adding a new interface

---
+++ SimpleAudioPlayer/Teddy.MvvmCross.Plugins.SimpleAudioPlayer/MvxSimpleAudioPlayerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teddy.MvvmCross.Plugins.SimpleAudioPlayer
+{
+    public static class SimpleAudioPlayerExtensions
+    {
+        public static void PlayOnRepeat(this IMvxSimpleAudioPlayer player)
+        {
+            player.Completion += new EventHandler(delegate (Object o, EventArgs a)
+            {
+                player.Play();
+            });
+
+            player.Play();
+        }
+    }
+}
