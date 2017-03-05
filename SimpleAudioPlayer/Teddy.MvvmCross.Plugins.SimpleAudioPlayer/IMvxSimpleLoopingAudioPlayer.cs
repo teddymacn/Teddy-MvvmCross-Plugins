858a792d66bfee6ffe614650eb1af6b66685891f
Matija Lukic <matija32@gmail.com>
2017-03-05 20:36:21 +0100

    added looping audio player adapter on top of an existing interface

---
+++ SimpleAudioPlayer/Teddy.MvvmCross.Plugins.SimpleAudioPlayer/IMvxSimpleLoopingAudioPlayer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teddy.MvvmCross.Plugins.SimpleAudioPlayer
+{
+    interface IMvxSimpleLoopingAudioPlayer
+    {
+        /// <summary>
+        /// Gets the current audio path.
+        /// </summary>
+        string Path { get;}
+
+        /// <summary>
+        /// Gets the duration of the audio in milliseconds.
+        /// </summary>
+        double Duration { get; }
+
+        /// <summary>
+        /// Gets the current position in milliseconds.
+        /// </summary>
+        double Position { get; }
+
+        /// <summary>
+        /// Whether or not it is playing.
+        /// </summary>
+        bool IsPlaying { get; }
+
+        /// <summary>
+        /// Gets or sets the current volume.
+        /// </summary>
+        double Volume { get; set; }
+
+        /// <summary>
+        /// Opens a specified audio path.
+        /// 
+        /// The following formats of path are supported:
+        ///     - Absolute URL, 
+        ///       e.g. http://abc.com/test.mp3
+        ///       
+        ///     - Assets Deployed with App, relative path assumed to be in the device specific assets folder
+        ///       Android and UWP relative to the Assets folder while iOS relative to the App root folder
+        ///       e.g. test.mp3
+        ///       
+        ///     - Local File System, arbitry local absolute file path the app has access
+        ///       e.g. /sdcard/test.mp3
+        /// </summary>
+        /// <param name="path">
+        ///     The audio path.
+        /// </param>
+        bool Open(string path);
+
+        /// <summary>
+        /// Plays the opened audio on loop.
+        /// </summary>
+        void PlayOnLoop();
+
+        /// <summary>
+        /// Stops playing.
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// Pauses the playing.
+        /// </summary>
+        void Pause();
+
+        /// <summary>
+        /// Seeks to specified position in milliseconds.
+        /// </summary>
+        /// <param name="pos">The position to seek to.</param>
+        void Seek(double pos);
+        
+    }
+}
---
+++ SimpleAudioPlayer/Teddy.MvvmCross.Plugins.SimpleAudioPlayer/MvxSimpleLoopingAudioPlayer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teddy.MvvmCross.Plugins.SimpleAudioPlayer
+{
+    public class MvxSimpleLoopingAudioPlayer : IMvxSimpleLoopingAudioPlayer
+    {
+        private IMvxSimpleAudioPlayer _player;
+
+        public MvxSimpleLoopingAudioPlayer(
+            IMvxSimpleAudioPlayer player)
+        {
+            _player = player;
+            SetupReplayOnCompletion();
+        }
+
+        public double Duration
+        {
+            get { return _player.Duration; }
+        }
+
+        public bool IsPlaying
+        {
+            get { return _player.IsPlaying; }
+        }
+
+        public string Path
+        {
+            get { return _player.Path; }
+        }
+
+        public double Position
+        {
+            get { return _player.Position; }
+        }
+
+        public double Volume
+        {
+            get { return _player.Volume; }
+            set { _player.Volume = value; }
+        }
+
+        public bool Open(string path)
+        {
+            return _player.Open(path);
+        }
+        
+        public void Pause()
+        {
+            _player.Pause();
+        }
+
+        public void PlayOnLoop()
+        {
+            _player.Play();
+        }
+
+        public void Seek(double pos)
+        {
+            _player.Seek(pos);
+        }
+
+        public void Stop()
+        {
+            _player.Stop();
+        }
+
+        private void SetupReplayOnCompletion()
+        {
+            _player.Completion += new EventHandler(delegate (Object o, EventArgs a)
+            {
+                _player.Play();
+            });
+        }
+        
+    }
+}
